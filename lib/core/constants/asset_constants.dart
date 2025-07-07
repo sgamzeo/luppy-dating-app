46d8359139b2414217fa73ab54e89f9e3ba79624
senaoz <ssenagamzee@gmail.com>
2025-07-08 00:31:53 +0300

    feat(assets): add onboarding illustrations and define AssetConstants

--- lib/core/constants/asset_constants.dart
+++ lib/core/constants/asset_constants.dart
@@ -1,1 +1,10 @@
-class AssetConstants {}
+class AssetConstants {
+  // onboarding
+  static const String groupOfDogs =
+      'assets/images/dog_illustration_group_of_dogs.png';
+  static const String dogCouple =
+      'assets/icons/dog_illustration_dog_couple.svg';
+  static const String dogPhone = 'assets/icons/dog_illustration_dog_phone.svg';
+  static const String welcomeDog =
+      'assets/icons/dog_illustration_welcome_dog.svg';
+}
