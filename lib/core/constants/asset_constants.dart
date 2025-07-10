b1724282e65401d9b82724f3fed3e7b37a4c361c
senaoz <ssenagamzee@gmail.com>
2025-07-10 12:36:35 +0300

    fix(core): update groupOfDogs asset path to SVG

--- lib/core/constants/asset_constants.dart
+++ lib/core/constants/asset_constants.dart
@@ -1,7 +1,7 @@
 class AssetConstants {
   // onboarding
   static const String groupOfDogs =
-      'assets/images/dog_illustration_group_of_dogs.png';
+      'assets/images/dog_illustration_group_of_dogs.svg';
   static const String dogCouple =
       'assets/icons/dog_illustration_dog_couple.svg';
   static const String dogPhone = 'assets/icons/dog_illustration_dog_phone.svg';
